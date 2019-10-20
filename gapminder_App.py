27711edb87f5877fe09c404a87aeedb4556b0dd9
duncandam <duncandam86@gmail.com>
2019-10-19 23:27:05 -0500

    update tabs

--- gapminder_App.py
+++ gapminder_App.py
@@ -9,6 +9,7 @@ import plotly.graph_objs as go
 
 app = dash.Dash(external_stylesheets=[dbc.themes.BOOTSTRAP])
 
+app.config.suppress_callback_exceptions = True
 
 #import data
 df = pd.read_csv('Data/gapminder_dd.csv')
@@ -26,20 +27,64 @@ navbar = dbc.NavbarSimple(
         brand_href="#",
         color="primary",
         dark=True
-    )
+)
 
 #body content for overview
+features = {
+    'Life Expectancy':'lifeExp',
+    'Child Mortality':'child_mortality',
+    'Income': 'income',
+    'Population': 'population',
+    'CO2 emission': 'co2_emission',
+    'Human development index': 'hdi',
+    'Number of HIV cases': 'hiv_num',
+}
+
 body_1 = dbc.Row ([
     dbc.Col([
-            html.H1('Selection goes here')
-        ],width = 4),
-        dbc.Col([
-            html.H2('Graph goes here')
-        ],width = 8)    
+            html.P('Select x-axis'),
+            dcc.Dropdown(
+                options = [
+                    {'label': k, 'value': v } for k,v in features.items()
+               ],
+               id='overview_xaxis',
+               value = 'lifeExp'
+            ),
+            html.Br(),
+            html.P('Select y-axis'),
+            dcc.Dropdown(
+                options = [
+                    {'label': k, 'value': v } for k,v in features.items()
+                ],
+                id='overview_yaxis',
+                value = 'income'
+            ),
+            html.Br(),
+            html.P('Choose year(s)'),
+            dcc.Dropdown(
+                options = [
+                    {'label': i, 'value': i } for i in range(1800,2019)
+               ],
+               id ='overview_year',
+               multi = True,
+               value = [2005, 2015]
+            ),
+    ],width = 3),
+        
+    dbc.Col([
+            dbc.Tabs(
+                id="tabs", 
+                active_tab = 'overview_visual',
+                children = [
+                    dbc.Tab(label='Data Visualization', tab_id='overview_visual'),
+                    dbc.Tab(label='Data Table', tab_id='overview_table')
+                ]
+            ),
+            html.Div(id='tab-content')
+    ],width = 9)    
 ])
 
 #body content for continent
-
 body_2 = dbc.Row ([
     dbc.Col([
             html.H1('Selection goes here for continent')
@@ -64,8 +109,6 @@ app.layout = html.Div([dcc.Location(id='url'),navbar, dbc.Container(id="page-con
 
 
 #Server setup
-
-
 
 #call back to activate toggle between pages/tabs
 @app.callback(
@@ -99,7 +142,21 @@ def render_page_content(pathname):
         ]
     )
 
+#call back for Tabs in Overview page
+@app.callback(
+    Output('tab-content', 'children'),
+    [Input('tabs', 'active_tab')],
+)
 
+def render_content(tab):
+    if tab == 'overview_visual':
+        return html.Div([
+            html.P('there will be a few graphs')
+        ])
+    elif tab == 'overview_table':
+        return html.Div([
+            html.P('there will be datatable')
+        ])
 
 #run app
 if __name__ == '__main__':
